93ab5ff0b7b6f9ae76004430339e691f9df55a19
kuramochi-coder <raphael.kw@gmail.com>
2021-06-03 10:31:41 +0800

    updated

--- network_delay_time.py
+++ network_delay_time.py
@@ -1,13 +1,19 @@
+# You are given a network of n nodes, labeled from 1 to n. 
+# You are also given times, a list of travel times as directed edges times[i] = (ui, vi, wi), 
+# where ui is the source node, vi is the target node, and wi is the time it takes for a signal to travel from source to target.
+# We will send a signal from a given node k. 
+# Return the time it takes for all the n nodes to receive the signal. 
+# If it is impossible for all the n nodes to receive the signal, return -1.
+
 import heapq
 
 # This solution uses Dijkstra's Shortest Path implemented with a binary min heap. 
 # Time Complexity: O(|E|log|E|), where |E| is the number of edges in the graph G = (V, E).
 class Solution:
     def networkDelayTime(self, times, n, k): # n is total number of nodes, k is the starting node
-        # implemented using dijkstra's shortest path
-        graph = {}  # create graph
         # times[i] = (ui, vi, wi), where ui is the source node, vi is the target node, 
         # and wi is the time it takes for a signal to travel from source to target.
+        graph = {}  # create graph
         for u, v, w in times:
             if u not in graph.keys():
                 graph[u] = [(v, w)]
