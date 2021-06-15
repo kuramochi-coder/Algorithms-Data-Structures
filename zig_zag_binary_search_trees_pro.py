42121627aa1a98dffd77544d0d2e75411b54da0b
kuramochi-coder <raphael.kw@gmail.com>
2021-06-15 22:30:17 +0800

    Updated code

--- zig_zag_binary_search_trees_pro.py
+++ zig_zag_binary_search_trees_pro.py
@@ -8,34 +8,70 @@ class Node:
         self.left = left
         self.right = right
 
+class Solution:
+    def zigzag_order(self, node):
+        result = []
+        currLevel = [node]
+        nextLevel = []
+        leftToRight = False
 
-def zigzag_order(node):
-    result = []
-    currLevel = [node]
-    nextLevel = []
-    leftToRight = False
+        while len(currLevel) > 0:
+            node = currLevel.pop()
+            result.append(node.value)
 
-    while len(currLevel) > 0:
-        node = currLevel.pop()
-        result.append(node.value)
+            if leftToRight:
+                if node.left:
+                    nextLevel.append(node.left)
+                if node.right:
+                    nextLevel.append(node.right)
+            if leftToRight != True:
+                if node.right:
+                    nextLevel.append(node.right)
+                if node.left:
+                    nextLevel.append(node.left)
 
-        if leftToRight:
-            if node.left:
-                nextLevel.append(node.left)
-            if node.right:
-                nextLevel.append(node.right)
-        if leftToRight != True:
-            if node.right:
-                nextLevel.append(node.right)
-            if node.left:
-                nextLevel.append(node.left)
+            if len(currLevel) == 0:
+                leftToRight = not leftToRight
+                currLevel = nextLevel
+                nextLevel = []
 
-        if len(currLevel) == 0:
-            leftToRight = not leftToRight
-            currLevel = nextLevel
-            nextLevel = []
+        return result
 
-    return result
+class Solution2:
+    def zigzagLevelOrder(self, root):
+        if root is None:
+            return []
+            
+        queue = [root]
+        current_level = 0
+
+        result = []
+        while len(queue) > 0:
+            queue_size = len(queue)
+            current_level += 1
+            level_list = []
+
+            for i in range(queue_size):
+                current = queue.pop(0)
+
+                level_list.append(current.value)
+                
+                if current.left:
+                    queue.append(current.left)
+                if current.right:
+                    queue.append(current.right)
+
+            if current_level % 2 == 0:
+                result.append(level_list)
+            else:
+                result.append(level_list[::-1])
+        
+        return result
+
+            
+
+
+
 
 
 n7 = Node(7)
@@ -46,4 +82,7 @@ n3 = Node(3, n6, n7)
 n2 = Node(2, n4, n5)
 n1 = Node(1, n2, n3)
 
-print(zigzag_order(n1))
+print(Solution().zigzag_order(n1))
+# [1, 2, 3, 7, 6, 5, 4]
+print(Solution2().zigzagLevelOrder(n1))
+# [1, 2, 3, 7, 6, 5, 4]
